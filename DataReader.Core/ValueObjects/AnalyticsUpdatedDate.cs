9546a581ee206061cd130fa9ec74cf47dde5754f
iwS <msp1108@mail.ru>
2024-07-18 19:46:24 +0200

    add comment to AnalyticsUpdatesDate and fill Log model

--- DataReader.Core/ValueObjects/AnalyticsUpdatedDate.cs
+++ DataReader.Core/ValueObjects/AnalyticsUpdatedDate.cs
@@ -6,6 +6,7 @@ namespace DataReader.Core.ValueObjects
 {
   public class AnalyticsUpdatedDate : ValueObject
   {
+    // 2023-05-31T11:56:25.2733333Z or 2024-05-16T11:10:41Z (ISO 8601)
     private const string REGEX_FOR_DATE = "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(\\.\\d+)?Z$";
     public string? Date { get; }
 
