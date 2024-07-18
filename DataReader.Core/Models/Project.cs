257fb023bcc46d608d53692e21f7f3f5db7c7a33
iwS <msp1108@mail.ru>
2024-07-18 19:05:39 +0200

    create Project model

---
+++ DataReader.Core/Models/Project.cs
@@ -0,0 +1,41 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace DataReader.Core.Models
+{
+  public class Project
+  {
+    private Project(
+      Guid projectSK,
+      Guid projectId,
+      string projectName,
+      string analyticsUpdatedDate,
+      string projectVisibility
+      )
+    {
+      ProjectSK = projectSK;
+      ProjectID = projectId;
+      ProjectName = projectName;
+      AnalyticsUpdatedDate = analyticsUpdatedDate;
+      ProjectVisibility = projectVisibility;
+    }
+
+    public Guid? ProjectSK {  get; }
+    public Guid? ProjectID { get; }
+    public string? ProjectName {  get; }
+    public string? AnalyticsUpdatedDate { get; }
+    public string? ProjectVisibility { get; }
+
+    public static Result<Project> Create(
+      Guid projectSK,
+      Guid projectId,
+      string projectName,
+      string analyticsUpdatedDate,
+      string projectVisibility
+      )
+    {
+      Project project = new Project(projectSK, projectId, projectName, analyticsUpdatedDate, projectVisibility);
+
+      return Result.Success(project);
+    }
+  }
+}
