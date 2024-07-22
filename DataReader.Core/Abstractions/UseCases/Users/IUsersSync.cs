97d919a90dda8a9b9ea953d3a9427727e9b39e00
iwS <msp1108@mail.ru>
2024-07-22 14:30:21 +0200

    upd IUserSync

--- DataReader.Core/Abstractions/UseCases/Users/IUsersSync.cs
+++ DataReader.Core/Abstractions/UseCases/Users/IUsersSync.cs
@@ -1,9 +1,10 @@
 ﻿using DataReader.Core.Contracts.Users;
 
+
 namespace DataReader.Application.SyncProcess
 {
   public interface IUsersSync
   {
-    Task Synchronisation(List<UsersRequest> requests);
+    Task Synchronisation(  requests);
   }
 }
