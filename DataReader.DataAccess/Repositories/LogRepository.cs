ee40007ea01d3d3d37aedc7891cb133872e25f1c
iwS <msp1108@mail.ru>
2024-08-01 00:00:51 +0200

    create implementation of Log repository

---
+++ DataReader.DataAccess/Repositories/LogRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataReader.Core.Abstractions.Repositories;
+
+
+namespace DataReader.DataAccess.Repositories
+{
+  public class LogRepository : ILogsRepository
+  {
+    private readonly DataDbContext _dbContext;
+
+    public LogRepository(DataDbContext dbContext)
+    {
+      _dbContext = dbContext;
+    }
+
+
+  }
+}
